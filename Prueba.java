ca93874abb6f8ff16eadfd6929ad51588847e167
paolagomeza <62312939+paolagomeza@users.noreply.github.com>
2020-06-23 10:45:32 -0500

    Add files via upload

---
+++ Prueba.java
@@ -0,0 +1,103 @@
+
+
+
+public class Prueba {
+	
+	public static void main (String[] args) {
+	arbol bonsai=new arbol();
+	bonsai.insertar(new Nodo(10));
+	if(bonsai.buscar(new Nodo(10))){
+	   System.out.println("El 10 si se encuentra ");
+	   	
+	}
+	Nodo AB ;
+	AB=new Nodo(0);
+	if(bonsai.buscar(AB)){
+	   System.out.println("El "+AB+"  si se encuentra ");
+     }else{
+	 System.out.println("El "+AB+" no se encuentra ");
+
+	 }
+	 	 ///////////////////////////////
+
+	 AB=new Nodo(5);
+	 bonsai.insertar(AB);
+	 if(bonsai.buscar(AB)){
+	   System.out.println("El "+AB+"  si se encuentra ");
+     }else{
+	 System.out.println("El "+AB+" no  se encuentra ");
+
+	 }	 ///////////////////////////////
+
+	  AB=new Nodo(15);
+	  	 bonsai.insertar(AB);
+
+	 if(bonsai.buscar(AB)){
+	   System.out.println("El "+AB+"  si se encuentra ");
+     }else{
+	 System.out.println("El "+AB+" no  se encuentra ");
+
+	 }
+	 	 ///////////////////////////////
+
+	 AB=new Nodo(4);
+	 	 bonsai.insertar(AB);
+
+	 if(bonsai.buscar(AB)){
+	   System.out.println("El "+AB+"  si se encuentra ");
+     }else{
+	 System.out.println("El "+AB+" no  se encuentra ");
+
+	 }
+	 	 ///////////////////////////////
+
+	 AB=new Nodo(16);
+	 	 bonsai.insertar(AB);
+
+	 if(bonsai.buscar(AB)){
+	   System.out.println("El "+AB+"  si se encuentra ");
+     }else{
+	 System.out.println("El "+AB+" no  se encuentra ");
+
+	 }
+	 	 ///////////////////////////////
+
+	  AB=new Nodo(8);
+	  	 bonsai.insertar(AB);
+
+	 if(bonsai.buscar(AB)){
+	   System.out.println("El "+AB+"  si se encuentra ");
+     }else{
+	 System.out.println("El "+AB+" no  se encuentra ");
+
+	 }
+	 ///////////////////////////////
+	  AB=new Nodo(11);
+	  	 bonsai.insertar(AB);
+
+	 if(bonsai.buscar(AB)){
+	   System.out.println("El "+AB+"  si se encuentra ");
+     }else{
+	 System.out.println("El "+AB+" no  se encuentra ");
+
+	 }
+	  AB=new Nodo(111);
+	  	 
+
+	 if(bonsai.buscar(AB)){
+	   System.out.println("El "+AB+"  si se encuentra ");
+     }else{
+	 System.out.println("El "+AB+" no  se encuentra ");
+
+	 }	 
+	 System.out.println("=============================");
+	 System.out.println("PreOrder");
+	 System.out.println(bonsai.toStringPreOrder());
+	 System.out.println("InOrder");
+	 System.out.println(bonsai.toStringinOrder());
+	 System.out.println("PosOrder");
+	 System.out.println(bonsai.toStringPostOrder());
+	 
+
+ }
+}
---
+++ arbol.java
@@ -0,0 +1,147 @@
+
+
+public class arbol {
+	private Nodo raiz;
+	private String Recorrido;
+	public arbol(){
+		raiz = null;
+		Recorrido="";
+	} 
+	//metodo a envolver
+	private void insertarDesde(Nodo noda, Nodo Desde){
+// Esto pasa cuando la raiz es nula
+	       // Es decir es el primer elemento
+	       
+		   if(Desde.obtenerData()==noda.obtenerData()){
+		   return;
+		   }
+		   if (noda.obtenerData()>Desde.obtenerData()){
+		      //colocar a la derecha
+		      if(Desde.Der==null){
+				  Desde.Der=noda;
+			  }else{
+				  //11
+				  insertarDesde(noda,Desde.Der);
+				  return;
+			   }
+			  }else{
+				  //colocar a la izquierda
+				  if(Desde.Izq==null){
+				     Desde.Izq=noda;
+				     return;
+				  }else{
+					  //11
+					  insertarDesde(noda,Desde.Izq);
+					  return;
+					}
+			  }
+		   }
+		   //metodo que envuelve wrapper
+		   public void insertar(Nodo noda){
+		   // Es decir es el primer elemento
+		   //esto pasa cuando la raiz es nula
+		   if(raiz==null){
+		      raiz=noda;
+		      return;
+		   }
+		   //si llego aqui la raiz no es nula
+		  // insertarDesde(noda,raiz);11
+		  insertarDesde(noda,raiz);	       
+		   }		
+	//}
+	
+	/////////////////////
+	public boolean buscar(Nodo noba){
+	    return bucarDesde(noba, raiz);
+	}
+	/////////////////////////////
+	private boolean bucarDesde(Nodo noba, Nodo Desde){
+		if (Desde==null){
+		     return false;
+		 }    
+		if(Desde.obtenerData()==noba.obtenerData()){
+		   return true;
+	   }
+	   if (noba.obtenerData()>Desde.obtenerData()){
+	   //derecha
+	   return bucarDesde (noba,Desde.Der);
+	   }else{
+		   //izquierda
+	   return bucarDesde (noba,Desde.Izq);
+
+		   //izquiera
+	   }
+	}
+	/////////////////////////7
+	public String toStringPreOrder(){
+	Recorrido="";
+	//////////////////////////
+	preOrderDesde(raiz);
+	/////////////////////////
+	return Recorrido;
+	}
+	//////////////////////////////////////
+	
+	/////////////////
+	
+	//////////////77
+	//tipo VID
+	private void preOrderDesde(Nodo Desde){
+		if(Desde ==null){
+         return;	
+		}
+		//valor
+		Recorrido=Recorrido+" "+Desde;
+		//Recorrido=Recorrido+" "+Desde.obtenerData
+        //IZQ
+        preOrderDesde(Desde.Izq);
+        //DERECHA
+        preOrderDesde(Desde.Der);		
+	}
+	////////////////////
+	public String toStringinOrder(){
+	Recorrido=" ";
+	//////////////////////////
+	inOrderDesde(raiz);
+	/////////////////////////
+	return Recorrido;
+	}
+	//IVD
+		private void inOrderDesde(Nodo Desde){
+		if(Desde ==null){
+			return;	
+		}
+			//inOrderDesde(Desde.Der);
+			inOrderDesde(Desde.Izq);
+			  Recorrido  =  Recorrido  +  " " +  Desde ;
+			inOrderDesde(Desde.Der);
+
+         
+			
+	}
+	///////////////////
+	public String toStringPostOrder(){
+	Recorrido="";
+	//////////////////////////
+	postOrderDesde(raiz);
+	/////////////////////////
+	return Recorrido;
+	}
+	//IDV
+		private void postOrderDesde(Nodo Desde){
+		if(Desde ==null){
+         return;	
+		}
+		//valor
+		//Recorrido=Recorrido+" "+Desde.obtenerData
+        //IZQ
+        postOrderDesde(Desde.Izq);
+        //DERECHA
+        postOrderDesde(Desde.Der);	
+        Recorrido= Recorrido+" " +  Desde ;
+	
+	}
+	
+	
+}
+
